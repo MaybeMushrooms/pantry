0e5124f4fb972f5d16df85c77b73d8ec69bb738d
maybemushrooms <thecraftedgem@tuta.io>
2018-07-25 22:38:09 -0600

    got alittle farter

--- lib/pantry.rb
+++ lib/pantry.rb
@@ -7,26 +7,30 @@ class Pantry
                       :add_to_shopping_list,
                       :shopping_list
     def initialize
-      @stock = {}
-      @stock_check = 0
+      @stock = Hash.new(0)
       @shopping_list = {}
-      @test_hash = {}
     end
 
     def stock_check(item)
-      @stock_check
+      @stock[item]
     end
     
     def restock(item, ammount)
-      @stock_check += ammount
+      @stock[item] += ammount
     end
 
     def add_to_shopping_list(recipe)
        list = recipe.ingredients
-       @shopping_list = @shopping_list.merge(list)
-     
+       @shopping_list = @shopping_list.merge(list) do |key, oldval, newval|
+        oldval + newval
+       end 
     end
-
+   
+    def print_shopping_list
+        @shopping_list.inject("") do |printed_list, pair| 
+            printed_list += "*#{pair.join(": ")}\n"
+        end 
+    end
     # patron.interests.sum do |interest|
     #     @exhibits[interest]
 end
--- test/pantry_test.rb
+++ test/pantry_test.rb
@@ -55,5 +55,24 @@ class PantryTest < Minitest::Test
      expected = {"Cheese" => 25, "Flour" => 20, "Spaghetti Noodles" => 10, "Marinara Sauce" => 10}
      assert_equal expected, pantry.shopping_list
     end
+     
+    def test_it_prints_shopping_list
+      r = Recipe.new("Cheese Pizza")
+      r.add_ingredient("Flour", 20) 
+      r.add_ingredient("Cheese", 20)
 
+      pantry = Pantry.new
+      pantry.add_to_shopping_list(r)
+
+      r = Recipe.new("Spaghetti")
+      r.add_ingredient("Spaghetti Noodles", 10)
+      r.add_ingredient("Marinara Sauce", 10)
+      r.add_ingredient("Cheese", 5)
+      pantry.add_to_shopping_list(r)
+      
+      expected = "* Cheese: 25\n* Flour: 20\n* Spaghetti Noodles: 10\n* Marinara Sauce: 10"
+      assert expected, pantry.print_shopping_list
+      
+    end
+    
 end
